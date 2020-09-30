5783ab0ff3d9c9cb63251b9b073bc1ff65aaaccd
Luzma <Luzma@DESKTOP-DKO00SJ>
2020-09-30 16:19:34 +0100

    nuevo 2

--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 public class Person {
     private final String name;
     private final LocalDate birthday;
+    private final String na;
 
     public Person(String name, LocalDate birthday) {
         this.name = name;
@@ -25,4 +26,5 @@ public class Person {
     private int toYear(long days) {
         return (int) (days/365);
     }
+    
 }
